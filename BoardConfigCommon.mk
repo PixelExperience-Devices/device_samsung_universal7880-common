1ed87b2a4a2039513f47cfcb88fdd3946388c1b2
faham1997 <nafidfaham08@gmail.com>
2020-08-15 09:16:10 +0000

    universal7880 : remove unessential twrp stuffs * we are not building twrp

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -208,29 +208,8 @@ TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE := true
 #TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)
 
 # Recovery
-#RECOVERY_VARIANT := twrp
 BOARD_HAS_DOWNLOAD_MODE := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/fstab.samsungexynos7880
-
-# TWRP
-ifeq ($(RECOVERY_VARIANT),twrp)
-PRODUCT_COPY_FILES += device/samsung/universal7880-common/twrp/twrp.fstab:recovery/root/etc/twrp.fstab
-TW_THEME := portrait_hdpi
-TW_BRIGHTNESS_PATH := /sys/class/backlight/panel/brightness
-TW_MAX_BRIGHTNESS := 255
-TW_DEFAULT_BRIGHTNESS := 162
-TW_NO_REBOOT_BOOTLOADER := true
-TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_NTFS_3G := true
-TW_HAS_DOWNLOAD_MODE := true
-TW_NO_EXFAT_FUSE := true
-TW_EXCLUDE_SUPERSU := true
-TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
-RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_REAL_SDCARD := true
-TW_USE_TOOLBOX=true
-TW_EXCLUDE_TWRPAPP := true
-endif
 
 # Seccomp filters
 BOARD_SECCOMP_POLICY += device/samsung/universal7880-common/seccomp
